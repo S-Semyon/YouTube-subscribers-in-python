352f102a66a3e6be23b35c63882eb2ca7873508d
Семён <96831996+S-Semyon@users.noreply.github.com>
2023-01-15 15:59:45 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ url = input("Insert a link to the channel: ")
 def subs(channel_url: str):
     with urllib.request.urlopen(channel_url) as response:
         html = response.read().decode("utf-8")
-    result = re.findall(r'subscriberCountText.*simpleText', str(html))
-    result = re.findall(r'label.*s', str(result))
+    result = re.findall(r'subscriberCountText.{0,150}simpleText', str(html))
+    result = re.search(r'label.*s', str(result[-1]))
     result = result[0][8:][:-6]
     return result
 
