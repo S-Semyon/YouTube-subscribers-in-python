3b9f0c6663dc8c2987ba525d547b297f2b8dd1e2
Семён <96831996+S-Semyon@users.noreply.github.com>
2023-01-15 16:00:54 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def subs(channel_url: str):
     with urllib.request.urlopen(channel_url) as response:
         html = response.read().decode("utf-8")
     result = re.findall(r'subscriberCountText.{0,150}simpleText', str(html))
-    result = re.search(r'label.*s', str(result[-1]))
-    result = result[0][8:][:-6]
+    result = re.search(r'label.*sim', str(result[-1]))
+    result = result[0][8:][:-8]
     return result
 
 print(subs(url))
